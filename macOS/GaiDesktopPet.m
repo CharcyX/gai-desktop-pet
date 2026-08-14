6a86b4be71a51c07c1ee3d0c87f115a7488c3d41
CharcyX <xuan3200200012@gmail.com>
2026-08-14 15:44:21 +0900

    Set macOS pet Dock offset to 70 pixels

--- macOS/GaiDesktopPet.m
+++ macOS/GaiDesktopPet.m
@@ -9,7 +9,7 @@ static const NSInteger Columns = 8;
 static const NSInteger Rows = 11;
 static const NSTimeInterval FrameStep = 0.14;
 static const CGFloat GazeMargin = 60.0;
-static const CGFloat DockGap = -30.0;
+static const CGFloat DockGap = -70.0;
 
 typedef NS_ENUM(NSInteger, PetState) {
     PetIdle, PetRap, PetSinging, PetReview, PetWaiting, PetFailed,
